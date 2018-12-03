96cfe481dfdf603d82875c5c979134806cf9c78d
yair halberstadt <yairhalberstadt@hotmail.co.uk>
2018-12-03 08:35:08 +0000

    Created Exception

---
+++ InvalidStateException/InvalidStateException.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace InvalidStateException
+{
+	public class InvalidStateException : Exception
+	{
+		public InvalidStateException()
+			: base(DefaultMessage)
+		{
+		}
+
+		public InvalidStateException(string message)
+			: base(Format(message))
+		{
+		}
+
+		public InvalidStateException(string message, Exception innerException)
+			: base(Format(message), innerException)
+		{
+		}
+
+		protected InvalidStateException(SerializationInfo info, StreamingContext context) : base(info, context)
+		{
+		}
+
+		private const string DefaultMessage =
+			"This Exception indicates an Invalid State has been reached, and should not be caught";
+		private static string Format(string message)
+		{
+			return $"{DefaultMessage}:\n{message}";
+		}
+	}
+}
